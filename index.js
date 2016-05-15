08380f7576e180bb34bfd07e7784ae542dab394a
Diego Rodríguez <diegorbaquero@gmail.com>
2016-05-15 16:57:59 -0500

    Get all fields

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ FBMessenger.prototype.getProfile = function (id, cb) {
     method: 'GET',
     uri: 'https://graph.facebook.com/v2.6/' + id,
     qs: {
-      fields: 'first_name,last_name,profile_pic',
+      fields: 'first_name,last_name,profile_pic,locale,timezone,gender',
       access_token: this.token
     },
     json: true
